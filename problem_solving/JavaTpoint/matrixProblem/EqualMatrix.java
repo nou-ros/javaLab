7c2d3b172be0f810a6006ac63bf36960a9ec6d32
nou-ros <nouros44@gmail.com>
2022-01-08 08:43:48 +0600

    added problem solve in the matrix

---
+++ problem_solving/JavaTpoint/matrixProblem/EqualMatrix.java
@@ -0,0 +1,43 @@
+public class EqualMatrix{
+	public static void main(String args[]){
+		int row1, row2, col1, col2;
+
+		int a[][] = { 
+			{1, 2, 3},
+			{8, 4, 6},
+			{4, 5, 7}			
+		};
+
+		int b[][] = {
+			{1, 2, 2},
+			{8, 4, 6},
+			{4, 5, 7}
+		};
+
+		row1 = a.length;
+		col1 = a[0].length;
+
+		row2 = b.length;
+		col2 = b[0].length;
+		boolean flag = true;
+
+		if(row1 != row2 || col1 != col2){
+			System.out.println("Matrices are not equal");
+		}
+		else{
+			for(int i = 0; i < row1; i++){
+				for(int j = 0; j < col1; j++){
+					if(a[i][j] != b[i][j]){
+						flag = false;
+						break;
+					}
+				}
+			}
+		}
+		if(flag)
+			System.out.println("Matrices are equal");
+		else
+			System.out.println("Matrices are not equal");
+
+	}
+}
