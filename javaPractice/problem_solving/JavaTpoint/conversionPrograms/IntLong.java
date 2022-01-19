6f404ae1458009be02b0324d35c9cd55e182f973
nou-ros <nouros44@gmail.com>
2022-01-19 10:59:13 +0600

    added to conversion programs

---
+++ javaPractice/problem_solving/JavaTpoint/conversionPrograms/IntLong.java
@@ -0,0 +1,20 @@
+public class IntLong{
+	public static void main(String args[]){
+		// long to int
+		long l = 500L;
+		int i = (int)l;
+		Long longNum = 100L;
+		int intNum = longNum.intValue();
+		System.out.println(i);
+		System.out.println(intNum);
+
+		//Int to Long
+		int val = 200;
+		long lval = val;
+		System.out.println(lval);
+		int val2 = 100;
+		Long lval2 = Long.valueOf(val2);
+		System.out.println(lval2);
+
+	}
+}
