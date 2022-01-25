b9488fff2d0d1202a4528126561665de37db4f3c
nou-ros <nouros44@gmail.com>
2022-01-25 17:03:52 +0600

    added programs in PatternPrograms

---
+++ javaPractice/problem_solving/JavaTpoint/patternProgram/PatternEight.java
@@ -0,0 +1,12 @@
+public class PatternEight{
+	public static void main(String args[]){
+		int i, j;
+		int p = 5;
+		for(i = 5; i > 0; i--){
+			for(j = 1; j <= i; j ++){
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+}	
