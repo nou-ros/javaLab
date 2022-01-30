efe0138ca4b0a273410aecbe104bf7039914147b
nou-ros <nouros44@gmail.com>
2022-01-30 09:46:24 +0600

    added IdenticalTrees program

---
+++ javaPractice/problem_solving/JavaTpoint/treePrograms/IdenticalTrees.java
@@ -0,0 +1,49 @@
+public class IdenticalTrees{
+	public static class Node{
+		int data;
+		Node left;
+		Node right;
+		
+		public Node(int data){
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+	public Node root;
+	public IdenticalTrees(){
+		root = null;
+	}
+
+	public static boolean areIdenticalTrees(Node root1, Node root2){
+		if(root1 == null && root2 == null)
+			return true;
+		if(root1 != null && root2 != null){
+			return ((root1.data == root2.data) && (areIdenticalTrees(root1.left, root2.left)) && (areIdenticalTrees(root1.right, root2.right)));
+		}
+		return false;
+	}
+
+	public static void main(String args[]){
+		IdenticalTrees bt1 = new IdenticalTrees();
+		bt1.root = new Node(1);
+		bt1.root.left = new Node(2);
+		bt1.root.right = new Node(3);
+		bt1.root.left.left = new Node(4);
+		bt1.root.right.left = new Node(5);
+		bt1.root.right.right = new Node(6);
+
+		IdenticalTrees bt2 = new IdenticalTrees();
+		bt2.root = new Node(1);
+		bt2.root.left = new Node(2);
+		bt2.root.right =new Node(3);
+		bt2.root.left.left = new Node(4);
+		bt2.root.right.left = new Node(7);
+		bt2.root.right.right = new Node(8);
+
+		if(areIdenticalTrees(bt1.root, bt2.root))
+			System.out.println("Both binary trees are identical");
+		else
+			System.out.println("Both binary trees are not identical");
+	}
+}
