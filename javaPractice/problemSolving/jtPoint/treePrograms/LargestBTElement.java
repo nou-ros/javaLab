08a0045c4de1a462da07764cc9e97af88c0c52bf
nou-ros <nouros44@gmail.com>
2022-02-01 09:06:28 +0600

    added largest binary tree element program

---
+++ javaPractice/problemSolving/jtPoint/treePrograms/LargestBTElement.java
@@ -0,0 +1,45 @@
+public class LargestBTElement{
+	public static class Node{
+		int data;
+		Node left;
+		Node right;
+		public Node(int data){
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+	public Node root;
+	public LargestBTElement(){
+		root = null;	
+	}
+	
+	public int largestElement(Node temp){
+		if(root == null){
+			System.out.println("Tree is empty");
+			return 0;
+		}else{
+			int leftMax, rightMax;
+			int max = temp.data;
+			if(temp.left != null){
+				leftMax = largestElement(temp.left);
+				max = Math.max(max, leftMax);
+			}
+			if(temp.right != null){
+				rightMax = largestElement(temp.right);
+				max = Math.max(max, rightMax);
+			}
+			return max;
+		}
+	}
+	public static void main(String args[]){
+		LargestBTElement bt = new LargestBTElement();
+		bt.root = new Node(15);
+		bt.root.left = new Node(20);
+		bt.root.right = new Node(35);
+		bt.root.left.left = new Node(74);
+		bt.root.right.left = new Node(55);
+		bt.root.right.right = new Node(6);
+		System.out.println("Largest element: " + bt.largestElement(bt.root));
+	}
+}
